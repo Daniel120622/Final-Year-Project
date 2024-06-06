29447ab3761fb3a962c1fbbad9f0e494119ee88d
Ling Ho Yin <115362380+Daniel120622@users.noreply.github.com>
2024-06-06 23:35:24 +0800

    Delete PythonScript.js

--- PythonScript.js
+++
@@ -1,6 +0,0 @@
-function runPythonScript(){
-    var PythonScriptPath = "C:\Users\lhy854a\PycharmProjects\FYP_callingChatGPT\DesignIdea.py";
-    subprocess.run(["python", pythonScriptPath]);
-}
-
-button.addEventListener("click", myFunction);
